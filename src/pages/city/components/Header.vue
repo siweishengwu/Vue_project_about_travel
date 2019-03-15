1dd283b7bad40375eadebada70058ae855c95fdc
siweishengwu <375782301@qq.com>
2019-03-15 09:44:01 +0800

    add code

--- src/pages/city/components/Header.vue
+++ src/pages/city/components/Header.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="header">
         城市选择
+        <router-link to="./">
+        <div class="iconfont header-back">&#xe624;</div>
+        </router-link>
     </div>
 </template>
 
@@ -11,13 +14,21 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-    @import '~styles/varibles.styl'
-    .header
-      overflow hidden
-      height  .86rem
-      line-height .86rem
+  @import '~styles/varibles.styl'
+  .header
+    overflow hidden
+    height  $headerHeight
+    line-height $headerHeight
+    text-align center
+    color #fff
+    background $bgColor
+    font-size .32rem
+    .header-back
+      position absolute
+      top 0
+      left 0
+      width  .64rem
       text-align center
+      font-size .4rem
       color #fff
-      background $bgColor
-      font-size .32rem
 </style>
